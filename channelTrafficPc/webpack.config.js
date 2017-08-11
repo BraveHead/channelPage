8d6833ba477c2cce7c00e1fab1d71c19360ce149
三页 <825171433@qq.com>
2017-08-11 15:03:54 +0800

    交通91.8的渠道页合作App和pc页面

---
+++ channelTrafficPc/webpack.config.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Yy on 2017/7/17.
+ */
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+module.exports = {
+    entry: ['./public/script/main.js'],
+    output: {
+        path: path.resolve('./public/script/'),
+        filename: 'main.bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                use: [
+                    {loader: 'babel-loader'},
+                ]
+            },
+
+            {
+                test:/\.css$/,
+                use:[ 'style-loader',
+                    {
+                        loader:'css-loader',
+                        options:{
+                            importLoaders: '1',
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return [
+                                    require('autoprefixer')({broswers:['last 5 versions']})
+                                ];
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: function(){
+                    return [
+                        require("autoprefixer")({
+                            browsers: ['ie>=8','>1% in CN']
+                        })
+                    ]
+                }
+            }
+        }),
+        new ExtractTextPlugin('./public/style.main.css'),
+        new UglifyJSPlugin(),
+    ]
+};
